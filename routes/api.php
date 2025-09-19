6231abd9817a7da9e47f71ee39294070844a9c03
alfin <muhamadalfinpratamaa@gmail.com>
2025-09-20 01:16:36 +0700

    refactor: simplify todos routes by using apiResource and remove redundant export route

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,7 @@ use App\Http\Controllers\TodoController;
 use App\Http\Controllers\ChartController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('todos')->group(function () {
-    Route::post('/', [TodoController::class, 'store']);
-    Route::get('/', [TodoController::class, 'index']);
-    Route::get('/{todo}', [TodoController::class, 'show']);
-    Route::put('/{todo}', [TodoController::class, 'update']);
-    Route::delete('/{todo}', [TodoController::class, 'destroy']);
-    Route::get('/export', [TodoController::class, 'export']);
-});
+Route::get('todos/export', [TodoController::class, 'export']);
+Route::get('chart', [ChartController::class, 'index']);
 
-Route::get('/chart', [ChartController::class, 'index']);
+Route::apiResource('todos', TodoController::class);
